3d0b48d3c86044e95f0b3f3a07999eba56e653bc
Kosrat Ahmed <kosrat.d.ahmad@gmail.com>
2025-06-24 16:03:12 +0300

    feat: Add support for rendering mermaid codes in the markdown files

--- astro.config.mjs
+++ astro.config.mjs
@@ -16,8 +16,12 @@ import rehypeSlug from "rehype-slug";
 import remarkDirective from "remark-directive"; /* Handle directives */
 import remarkGithubAdmonitionsToDirectives from "remark-github-admonitions-to-directives";
 import remarkMath from "remark-math";
+import remarkMermaid from "remark-mermaidjs";
 import remarkSectionize from "remark-sectionize";
-import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-copy-button.js";
+import {
+	pluginCustomCopyButton,
+	pluginCustomCopyButton,
+} from "./src/plugins/expressive-code/custom-copy-button.js";
 import { AdmonitionComponent } from "./src/plugins/rehype-component-admonition.mjs";
 import { GithubCardComponent } from "./src/plugins/rehype-component-github-card.mjs";
 import { parseDirectiveNode } from "./src/plugins/remark-directive-rehype.js";
@@ -112,6 +116,7 @@ export default defineConfig({
 			remarkDirective,
 			remarkSectionize,
 			parseDirectiveNode,
+			remarkMermaid,
 		],
 		rehypePlugins: [
 			rehypeKatex,
